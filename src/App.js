361adf6ded2ce1c4c10949bbe5d3d93d1bb4bc6d
George Bastawros <george.bastawros@outlook.com>
2022-11-06 23:26:09 -0600

    final

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 //import './App.css';
 import TopBar from './components/TopBar';
 import Background from './components/Background';
-
+import StoreListings from './components/StoreListings'
 
 function App() {
   return (
     <div>
       <TopBar />
       <Background />
+      <StoreListings />
     </div>
   );
 }
--- src/components/Background.js
+++ src/components/Background.js
@@ -2,27 +2,29 @@ import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form'
 
 function Background() {
-  return (
-    <>
-    <Card style={{ width: "1300px", minHeight: "428px", marginBottom: "50px", backgroundColor: "#DEEED6" }} className="text-black">
-      <Card.Img src="https://www.instacart.com/image-server/x428/www.instacart.com/assets/homepage/homepage_background_full_m3_cropped-8d2d286263821da7decd7c61fb1db1eb0e3dec13e0c356277d6d3cb7484c024a.jpg" 
-                alt="Card image"
-                style={{width: "%", height:"270px"}}
-      />
-      <Card.ImgOverlay>
-        <Card.Text>
-          <h1 style={{textAlign: "center"}}>Order groceries for delivery or pickup today</h1>
-        </Card.Text>
-      </Card.ImgOverlay>
-    
-    <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Control size="lg" type="text" placeholder="Enter your address" />
-      </Form.Group>
-    </Form>
-    </Card>
-    </>
-  );
+    return (
+        <>
+            <Card style={{ width: "1300px", minHeight: "428px", marginBottom: "50px", backgroundColor: "#DEEED6" }} className="text-black">
+                <Card.Img style={{ height: "100%", width: "50%", position: "absolute"}}
+                    src="https://www.instacart.com/image-server/x428/www.instacart.com/assets/homepage/homepage_background_full_m3_cropped-8d2d286263821da7decd7c61fb1db1eb0e3dec13e0c356277d6d3cb7484c024a.jpg"
+                    alt="Card image"
+                />
+                <Card.ImgOverlay style={{ top: "auto", width: "578px", marginLeft: "100px", marginBottom: "50px" }}>
+                    <Card.Text style={{ fontSize: "2.5em", fontWeight: "bold" }}>
+                        Order groceries for delivery <br /> or pickup today
+                    </Card.Text>
+                    <Card.Text style={{ fontSize: "1em" }}>
+                        Whatever you want from local stores, brought right to your door.
+                    </Card.Text>
+                    <Form>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Control size="lg" type="text" placeholder="Enter your address" />
+                    </Form.Group>
+                </Form>
+                </Card.ImgOverlay>
+            </Card>
+        </>
+    );
 }
 
 export default Background
